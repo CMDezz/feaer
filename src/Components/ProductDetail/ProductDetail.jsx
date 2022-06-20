609c39b9ab233caa955b858e9064cdac746ac5b4
CMDezz <levietviet44266@gmail.com>
2022-06-20 12:32:54 +0700

    push

--- src/Components/ProductDetail/ProductDetail.jsx
+++ src/Components/ProductDetail/ProductDetail.jsx
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
-import "./ProductDetail.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
+// import SwiperCore, { Pagination } from "swiper/core";
+import "./ProductDetail.scss";
 import "swiper/css";
+
 import "swiper/css/pagination";
+// SwiperCore.use([Pagination]);
 import { Pagination } from "swiper";
 
 const ProductDetail = (props) => {
+  var menu = ["Slide 1", "Slide 2", "Slide 3"];
+
+  // el: ".swiper-pagination",
+  // type: "custom",
+  // renderCustom: function (swiper, current, total) {
+  //   var customPaginationHtml = "";
+  //   for (var i = 0; i < total; i++) {
+  //     //Determine which pager should be activated at this time
+  //     if (i == current - 1) {
+  //       customPaginationHtml +=
+  //         '<span class="swiper-pagination-customs swiper-pagination-customs-active"></span>';
+  //     } else {
+  //       customPaginationHtml +=
+  //         '<span class="swiper-pagination-customs"></span>';
+  //     }
+  //   }
+  //   return customPaginationHtml;
+  // },
   let renderProductDetailSlider = () => {
     if (props.dataProductDetail.ImageDetail != undefined) {
       return props.dataProductDetail.ImageDetail.map((i) => {
@@ -21,15 +42,21 @@ const ProductDetail = (props) => {
     <div className="ProductDetail">
       <div className="ProductDetailSlider">
         <Swiper
-          direction={"vertical"}
-          pagination={{
-            clickable: true,
-          }}
-          height={"600"}
           modules={[Pagination]}
-          className="mySwiper"
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{
+            el: ".swiper-pagination",
+            clickable: true,
+            renderCustom: function (index, className) {
+              return (
+                '<span class="' + className + '">' + menu[index] + "</span>"
+              );
+            },
+          }}
         >
           {renderProductDetailSlider()}
+          <div className="swiper-pagination swiper-pagination-timeline-page" />
         </Swiper>
       </div>
       <div className="ProductDetailInfo"></div>
